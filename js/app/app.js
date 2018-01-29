e61cbd89288dfc79588dd0774b09678e7196b778
Declan Li-Carney <21116960@tasfiledomain.tas.edu.tw>
2018-01-29 12:43:51 +0800

    intitial push

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,7 @@
 var startApp = function() {
-  // alert('started');
+  cordova.plugins.notification.local.schedule({
+    title: 'My first notification',
+    text: 'Thats pretty easy...',
+    foreground: true
+  });
 };
